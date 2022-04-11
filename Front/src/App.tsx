9d3a072c547d0ec6498ea5660ff8eedfd9e78290
me-hana <hayonleee@gmail.com>
2022-04-11 22:39:19 +0900

    [FEAT] add first page

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import FirstIndex from "components/first/FirstIndex";
 
 function App() {
+  const [sketchbook, setSketchbook] = useState(0);
+
+  const switchSketchbook = (s: number) => {
+    switch (s) {
+      case 1:
+        return <FirstIndex />
+      default:
+        return <div>버튼을 눌러</div>
+    }
+  }
+
   return (
     <>
-      <div>This is Hannah's playground</div>
+      <div className="button-area">
+        <button onClick={()=>setSketchbook(1)}>first</button>
+        <button>second</button>
+        <button>third</button>
+        <button>fourth</button>
+      </div>
+      
+      <div className="sketch-book">
+        {switchSketchbook(sketchbook)}
+      </div>
     </>
   );
 }
---
+++ Front/src/components/first/FirstIndex.tsx
@@ -0,0 +1,7 @@
+const FirstIndex = () => {
+    return (
+        <div>첫번째!</div>
+    )
+}
+
+export default FirstIndex;

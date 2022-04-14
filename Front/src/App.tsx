6146006ef9ea9c098efa23758290bcb6b605ea1b
me-hana <hayonleee@gmail.com>
2022-04-14 09:01:24 +0900

    [FEAT] add second page

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import FirstIndex from "components/first/FirstIndex";
+import SecondIndex from "components/second/SecondIndex";
 
 function App() {
   const [sketchbook, setSketchbook] = useState(0);
@@ -7,24 +8,24 @@ function App() {
   const switchSketchbook = (s: number) => {
     switch (s) {
       case 1:
-        return <FirstIndex />
+        return <FirstIndex />;
+      case 2:
+        return <SecondIndex />;
       default:
-        return <div>버튼을 눌러</div>
+        return <div>버튼을 눌러</div>;
     }
-  }
+  };
 
   return (
     <>
       <div className="button-area">
-        <button onClick={()=>setSketchbook(1)}>first</button>
-        <button>second</button>
+        <button onClick={() => setSketchbook(1)}>first</button>
+        <button onClick={() => setSketchbook(2)}>second</button>
         <button>third</button>
         <button>fourth</button>
       </div>
-      
-      <div className="sketch-book">
-        {switchSketchbook(sketchbook)}
-      </div>
+
+      <div className="sketch-book">{switchSketchbook(sketchbook)}</div>
     </>
   );
 }
---
+++ Front/src/components/second/SecondIndex.tsx
@@ -0,0 +1,5 @@
+const SecondIndex = () => {
+  return <div>두번째!</div>;
+};
+
+export default SecondIndex;

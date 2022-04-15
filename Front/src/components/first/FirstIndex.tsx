f292c9145a68b8487233e48a658f3cd8b2755e1a
me-hana <hayonleee@gmail.com>
2022-04-15 16:39:16 +0900

    [FEAT] add emotion and dummy data file

--- Front/src/components/first/FirstIndex.tsx
+++ Front/src/components/first/FirstIndex.tsx
@@ -1,51 +1,100 @@
 import React, { useCallback, useRef, useState } from "react";
+import { styled } from "@mui/material/styles";
 import { AgGridReact } from "ag-grid-react";
-
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
+import _data from "./_data";
+import { Button } from "@mui/material";
+import { CheckboxSelectionCallbackParams } from "ag-grid-community";
+
+const ContentWrapper = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: "#FCC",
+}));
 
 const FirstIndex = () => {
-  const [rowData] = useState([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 },
-  ]);
+  const [rowData, setRowData] = useState(_data);
+  const [mode, setMode] = useState<"select" | "drag">("select");
   const gridRef = useRef<any>();
 
-  const [columnDefs] = useState([
-    // { field: "make", sortable: true, filter: true, checkboxSelection: true },
-    { field: "make", rowDrag: true },
-    { field: "model" },
-    { field: "price" },
+  const [columnDefs, setColumnDefs] = useState([
+    // { field: "name", sortable: true, filter: true, checkboxSelection: true },
+    {
+      field: "name",
+      //   rowDrag: (params: any) => (mode === "drag" ? true : false),
+      checkboxSelection: true,
+    },
+    { field: "sex" },
+    { field: "age" },
+    { field: "job" },
   ]);
 
-  const onButtonClick = (e: any) => {
+  const clickMode = () => {
+    if (mode === "select") {
+      setMode("drag");
+      console.log("지금 모드", mode);
+    } else if (mode === "drag") {
+      setMode("select");
+      console.log("지금 모드", mode);
+    } else {
+      console.log("TYPE ERROR 1");
+    }
+  };
+
+  const clickSelect = (e: any) => {
     const selectedNodes = gridRef.current?.api.getSelectedNodes();
     const selectedData = selectedNodes.map((node: any) => node.data);
     const selectedDataStringPresentation = selectedData
-      .map((node: any) => `${node.make} ${node.model}`)
+      .map((node: any) => `${node.name} ${node.age}`)
       .join(", ");
     alert(`Selected nodes: ${selectedDataStringPresentation}`);
   };
 
+  const clickAll = () => {
+    console.log("지금 구조는?");
+  };
+
   const onRowDragEnd = useCallback((e) => {
-    console.log("onRowDragEnd", e.node.data);
+    console.log("onRowDragEnd", e.node);
   }, []);
 
   return (
-    <div className="ag-theme-balham" style={{ height: 400 }}>
-      {/* <button onClick={onButtonClick}>Get selected rows</button> */}
-      <AgGridReact
-        rowData={rowData}
-        // ref={gridRef}
-        columnDefs={columnDefs}
-        // rowSelection="multiple"
-        // rowMultiSelectWithClick={false}
-        rowDragManaged={true}
-        animateRows={true}
-        onRowDragEnd={onRowDragEnd}
-      ></AgGridReact>
-    </div>
+    <ContentWrapper>
+      <div className="button-area">
+        <Button variant="contained" color="error" onClick={clickMode}>
+          모드 변경
+        </Button>
+        <Button variant="contained" onClick={clickSelect}>
+          선택한 행 보기
+        </Button>
+        <Button variant="contained" onClick={clickAll}>
+          행 추가하기
+        </Button>
+        <Button variant="contained" onClick={clickAll}>
+          지금 구조 보기
+        </Button>
+      </div>
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: "40vh",
+          width: "60vw",
+        }}
+      >
+        <AgGridReact
+          rowData={rowData}
+          ref={gridRef}
+          columnDefs={columnDefs}
+          rowSelection="multiple"
+          rowMultiSelectWithClick={false}
+          rowDragManaged={mode === "drag" ? true : false}
+          animateRows={mode === "drag" ? true : false}
+          onRowDragEnd={onRowDragEnd}
+        ></AgGridReact>
+      </div>
+    </ContentWrapper>
   );
 };
 
---
+++ Front/src/components/first/_data.ts
@@ -0,0 +1,9 @@
+const _data = [
+  { row_id: 1, name: "이하연", sex: "Female", age: 27, job: "Developer" },
+  { row_id: 2, name: "서강준", sex: "Male", age: 28, job: "Actor" },
+  { row_id: 3, name: "장원영", sex: "Female", age: 17, job: "Singer" },
+  { row_id: 4, name: "김영한", sex: "Male", age: 38, job: "Developer" },
+  { row_id: 5, name: "김연아", sex: "Female", age: 31, job: "Athlete" },
+];
+
+export default _data;
